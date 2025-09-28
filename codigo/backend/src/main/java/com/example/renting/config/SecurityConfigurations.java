3dd3f83b3d1175b5a9ab4d3e5449e69fc5fe4499
Matheus Gaston <theteusviana@gmail.com>
2025-09-28 17:52:56 -0300

    update: cors config

--- codigo/backend/src/main/java/com/example/renting/config/SecurityConfigurations.java
+++ codigo/backend/src/main/java/com/example/renting/config/SecurityConfigurations.java
@@ -29,7 +29,7 @@ public class SecurityConfigurations {
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(request -> {
                     var corsConfig = new org.springframework.web.cors.CorsConfiguration();
-                    corsConfig.setAllowedOrigins(List.of("http://localhost:3000"));
+                    corsConfig.setAllowedOrigins(List.of("https://aluguel-de-carros-production.up.railway.app"));
                     corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     corsConfig.setAllowedHeaders(List.of("*"));
                     corsConfig.setAllowCredentials(true);
